344859d64f3faef23e0ddb868752fba515416117
Declan O'Mara <25646795+declanomara@users.noreply.github.com>
2019-01-29 17:24:52 +0000

    Initial commit of coinflip algorithm

---
+++ coinflip.py
@@ -0,0 +1,11 @@
+
+
+# Algorithm
+# Flip a coin, pick either up or down
+# Set stop loss and buy stop
+# Repeat
+
+# TODO
+# Implement algo
+# Implementing trailing stop loss and buy stop
+# Optimize
