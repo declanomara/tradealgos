8a00e3952051aec65af33220f000d108f08cca00
declan <25646795+declanomara@users.noreply.github.com>
2019-01-29 21:54:52 -0500

    Implemented Pandas based data loading -- current fastest working implementation

--- dataloader.py
+++ dataloader.py
@@ -1,14 +1,45 @@
 # Used to load price data in order to run simulation on historical price data
 import csv
+import time
+import numpy as np
+import pandas as pd
+
+def timeit(f):
+
+    def timed(*args, **kw):
+
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+
+        print('func:%r args:[%r, %r] took: %2.4f sec' %
+          (f.__name__, args, kw, te-ts))
+        return result
+
+    return timed
 
 
 def load_data_from_csv(file):
     with open(file, 'r') as f:
         reader = csv.reader(f)
 
-        for row in reader:
-            print(row)
+        data = [x for x in reader]
+        return data
+
+
+def load_data_pd_read_csv(file):
+    names = ['time', 'price', 'unsure']
+    return pd.read_csv(file, header=None, names=names)
+
+
+def load_data_np_fromfile(file):
+    with open(file, 'r') as f:
+        print(np.fromfile(f, dtype=str, count=-1, sep=','))
 
 
 if __name__ == '__main__':
-    load_data_from_csv('krakenUSD.csv')
+    f = input('Price data file name: ')
+    load = timeit(load_data_pd_read_csv)
+    data = load(f)
+    for row in data.itertuples():
+        print(row)
